d53e71de0ca47cadc3b3d3d3fc769b0de01e6272
Egor Avakumov <avakymov21@gmail.com>
2023-09-21 18:25:33 +0300

    fix: remove branch

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('promo');
+import { $ } from 'execa';
+
+const run = async (): Promise<void> => {
+  try {
+    // Создаем новую директорию "test" и инициализируем npm проект
+    await $`mkdir test && cd test && npm init -y`;
+  } catch (e: any) {
+    console.error(e);
+  }
+};
+
+run().catch((e) => {
+  console.error(e);
+});
