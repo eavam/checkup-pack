9d425cedca72c6391c6e212e7657ce9cfecf5c8b
Egor Avakumov <avakymov21@gmail.com>
2023-09-23 14:15:48 +0300

    fix: linter

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const run = async (): Promise<void> => {
   try {
     // Создаем новую директорию "test" и инициализируем npm проект
     await $`mkdir test && cd test && npm init -y`;
-  } catch (e: any) {
+  } catch (e) {
     console.error(e);
   }
 };
