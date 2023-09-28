bbb065aa9cf287826d05b313587b2a1d37e7bab3
Egor Avakumov <avakymov21@gmail.com>
2023-09-28 22:06:20 +0300

    chore: add ts-node

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { $ } from 'execa';
 
+const cleanup = async (): Promise<void> => {
+  await $`cd ../`;
+  await $`rm -rf test`;
+};
+
 const run = async (): Promise<void> => {
   try {
     // Создаем новую директорию "test" и инициализируем npm проект
-    await $`mkdir test && cd test && npm init -y`;
+    await $`mkdir test`;
+    await $`cd test`;
+    await $`npm init -y`;
+    // await cleanup();
   } catch (e) {
     console.error(e);
   }
